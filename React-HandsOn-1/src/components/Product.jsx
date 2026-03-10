147ad5f5e11886a2edd1ed1d79d66297c9b615bc
bigghosslentilocsta <pavankumar111205@gmail.com>
2026-03-10 21:11:45 +0530

    updating all the 3 phases

--- React-HandsOn-1/src/components/Product.jsx
+++ React-HandsOn-1/src/components/Product.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function Product({name, price, brand, description, image}){
     
 return(
-    <div className="w-[400px] mx-auto border-2 border-red-400 rounded-lg shadow-sky-50 p-5 flex flex-col items-center m-10">
+    <div className="w-100 mx-auto border-2 border-red-400 rounded-lg shadow-sky-50 p-5 flex flex-col items-center m-10">
         <div className="flex justify-center items-center h-50 w-full p-5 ">
   <img src={image} className="h-full object-contain" alt={name} />
 </div>
